6f386810880c05367502d889863fdfb1ebd886a5
skhsceye <skh@sceye.com>
2021-02-11 23:26:15 -0700

    Updated analysis with more functions

--- analysis.py
+++ analysis.py
@@ -48,17 +48,23 @@ def perform_analysis(data, d_name):
     x_bar = np.average(data)
     S = np.std(data, ddof=1) #Sets divisor to N-ddof or N-1
     S_xbar = S/ np.sqrt(N)
+
+    X = 0
+    for i in range(N):
+        X += ((data[i] - x_bar)/(S))**2
+
     print("N", N , d_name)
     print("x_bar ", x_bar, d_name)
     print("S ", S, d_name)
     print("S_xbar", S_xbar, d_name)
+    print("X", X, d_name)
 
     return(N, x_bar, S, S_xbar)
 
 data_tuple = perform_analysis(data, plot_y_label)
 
 
-fig, ax = plt.subplots(1,1)
+fig, ax = plt.subplots(1,2)
 
 def plot_sample(ax, data, dtuple):
 
@@ -74,6 +80,7 @@ def plot_sample(ax, data, dtuple):
     ax.set_title(plot_title)
 
 
+
     # ax.set_ylim(0.75, 1.5)
     av_line = ax.hlines(x_bar, 0, N-1, color="red", label="mean")
 
@@ -81,9 +88,43 @@ def plot_sample(ax, data, dtuple):
     std_below = ax.hlines(x_bar-S, 0, N-1, color="blue", linestyle="--", label="mean - std")
     ax.legend()
 
+def plot_difference_plot(ax, data, dtuple):
+    global plot_x_label
+    global plot_y_label
+    global plot_title
 
+    plot_title = "Deviation Plot"
+    plot_y_label = "Sigma"
+
+    N, x_bar, S, S_xbar = dtuple
+    x = np.arange(N)
+
+    diffav = lambda p: (p-x_bar)/S
+    data = np.array([diffav(xi) for xi in data])
+
+    S -= x_bar
+    ax.scatter(x, data, s=35, marker="o", color="red")
+    ax.set_xlabel(plot_x_label)
+    ax.set_ylabel(plot_y_label)
+    ax.set_title(plot_title)
+
+
+
+    # ax.set_ylim(0.75, 1.5)
+    av_line = ax.hlines(0, 0, N-1, color="red", label="mean")
+
+    for i in range(1,4):
+        std_above = ax.hlines(0+i, 0, N-1, color=(0, 0.1, 1, 1/(i)), linestyle="--", label="mean + std")
+        std_below = ax.hlines(0-i, 0, N-1, color=(0, 0.1, 1, 1/(i)), linestyle="--", label="mean - std")
+        if (i > 1):
+            std_above.set_label(None)
+            std_below.set_label(None)
+
+    ax.legend()   
 #Sample Plots
-plot_sample(ax, data, data_tuple)
+
+plot_sample(ax[0], data, data_tuple)
+plot_difference_plot(ax[1], data, data_tuple)
 # plot_sample(ax, y_vals, h_tuple)
 
 # # N is the count in each bin, bins is the lower-limit of the bin
